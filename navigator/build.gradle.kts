c58c471cf407814715b2849afb07ee7bed3c73c3
Mulledwineapps <mulledwineapps@yandex.ru>
2024-08-05 10:45:37 +0300

    Use version catalog for detekt

--- navigator/build.gradle.kts
+++ navigator/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("io.gitlab.arturbosch.detekt")
+    alias(libs.plugins.detekt)
     id("maven-publish")
 }
 
